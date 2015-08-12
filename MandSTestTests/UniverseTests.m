d40caf37338dc40392f9b769b8397c741e891e86
Trevor Doodes <trevor@ironworksmedialimited.co.uk>
2015-08-12 19:03:05 +0100

    Created Universe Teat fixture

--- MandSTestTests/UniverseTests.m
+++ MandSTestTests/UniverseTests.m
@@ -1,6 +1,6 @@
 //
-//  MandSTestTests.m
-//  MandSTestTests
+//  UniverseTests.m
+//  MandSTest
 //
 //  Created by Trevor Doodes on 12/08/2015.
 //  Copyright (c) 2015 Ironworks Media Limited. All rights reserved.
@@ -9,11 +9,11 @@
 #import <UIKit/UIKit.h>
 #import <XCTest/XCTest.h>
 
-@interface MandSTestTests : XCTestCase
+@interface UniverseTests : XCTestCase
 
 @end
 
-@implementation MandSTestTests
+@implementation UniverseTests
 
 - (void)setUp {
     [super setUp];
