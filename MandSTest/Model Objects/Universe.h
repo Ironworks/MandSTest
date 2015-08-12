9e7d2e194a9ecb7aa4bd5d51e980bf3342c88fa9
Trevor Doodes <trevor@ironworksmedialimited.co.uk>
2015-08-12 19:31:27 +0100

    Created Universe Class

---
+++ MandSTest/Model Objects/Universe.h
@@ -0,0 +1,13 @@
+//
+//  Universe.h
+//  MandSTest
+//
+//  Created by Trevor Doodes on 12/08/2015.
+//  Copyright (c) 2015 Ironworks Media Limited. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Universe : NSObject
+
+@end
---
+++ MandSTest/Model Objects/Universe.m
@@ -0,0 +1,13 @@
+//
+//  Universe.m
+//  MandSTest
+//
+//  Created by Trevor Doodes on 12/08/2015.
+//  Copyright (c) 2015 Ironworks Media Limited. All rights reserved.
+//
+
+#import "Universe.h"
+
+@implementation Universe
+
+@end
