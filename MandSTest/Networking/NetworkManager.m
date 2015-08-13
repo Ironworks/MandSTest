00d1c5470e880bcde583e8bddf5ffd719ba8db3f
Trevor Doodes <trevor@ironworksmedialimited.co.uk>
2015-08-14 00:02:31 +0100

    Implement get probe navigation data

--- MandSTest/Networking/NetworkManager.m
+++ MandSTest/Networking/NetworkManager.m
@@ -8,7 +8,55 @@
 
 #import "NetworkManager.h"
 
+NSString * const kBaseURL = @"https://mands-alien-test.herokuapp.com/api/spaceprobe/";
+
+
+typedef NS_ENUM(NSUInteger, HTTPStatusCode) {
+    HTTPStatusCodeOK = 200,
+    HTTPStatusCodePostOK = 201,
+    HTTPStatusCodeBadRequest   = 400,
+    HTTPStatusCodeUnauthorised = 401,
+    HTTPStatusCodeForbidden    = 403,
+    HTTPStatusCodeNotFound     = 404,
+    HTTPStatusCodeInternalError = 500
+};
+
+
+
 @implementation NetworkManager
+
+
+/**
+ *  Create user session
+ *
+ *  @return NSURLSession
+ */
+- (NSURLSession *)session
+{
+    
+    NSURLSessionConfiguration *sessionConfig = [NSURLSessionConfiguration defaultSessionConfiguration];
+    [sessionConfig setHTTPAdditionalHeaders:@{@"Content-Type" : @"application/json",
+                                              @"Accept" : @"application/json"}];
+    
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:sessionConfig];
+    return session;
+}
+
+/**
+ *  Setup mutable GET request for urlString
+ *
+ *  @param urlString String representation of the rquested URL
+ *
+ *  @return mutable GET request
+ */
+- (NSMutableURLRequest *)mutableRequestForURLString:(NSString *)urlString
+{
+    NSMutableURLRequest *request = [[NSMutableURLRequest alloc]
+                                    initWithURL:[NSURL URLWithString:urlString]];
+    return request;
+}
+
+
 
 /**
  *  Get the probe navigation data
@@ -20,6 +68,65 @@
 - (void) probeNavigationGetDataForEmail:(NSString *) email
                          requestSuccess:(getDirectionsSuccess)success
                          requestFailure:(requestFailed)failure {
+    
+    
+    NSParameterAssert(email);
+    
+    //Create session
+    NSURLSession *session = [self session];
+    
+    NSString *urlString = [NSString stringWithFormat:@"%@getdata/%@", kBaseURL, email];
+    
+    NSMutableURLRequest *request = [self mutableRequestForURLString:urlString];
+    
+    [[session dataTaskWithRequest:request
+                completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+                    
+                    NSError *jsonError;
+                    NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data
+                                                                         options:NSJSONReadingAllowFragments
+                                                                           error:&jsonError];
+                    //Check status code for request
+                    NSHTTPURLResponse *httpResp = (NSHTTPURLResponse *)response;
+                    if (httpResp.statusCode == HTTPStatusCodeOK) {
+                        
+                        if (json[@"Directions"]) {
+                            NSArray *directions = json[@"Directions"];
+                            
+                            dispatch_async(dispatch_get_main_queue(), ^{
+                                if (success) {
+                                    success(directions);
+                                }
+                            });
+                            
+                        }
+                        
+                    } else if (httpResp.statusCode == HTTPStatusCodeInternalError) {
+                        
+                        if (json[@"Message"]) {
+                            NSString *message = json[@"Message"];
+                            NSNumber *statusCode = json[@"StatusCode"];
+                            
+                            dispatch_async(dispatch_get_main_queue(), ^{
+                                if (failure) {
+                                    failure(message, [statusCode integerValue]);
+                                }
+                            });
+
+                        }
+                        
+                    } else {
+                        
+                        //Something else has gone wrong
+                        dispatch_async(dispatch_get_main_queue(), ^{
+                            if (failure) {
+                                failure(@"Unknown error occured", 999);
+                                
+                            }
+                        });
+                    }
+                    
+                }] resume];
     
     
     
