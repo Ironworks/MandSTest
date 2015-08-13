3e804b0f86ab2b4390c4cfd695112a78e63c5b04
Trevor Doodes <trevor@ironworksmedialimited.co.uk>
2015-08-13 13:30:15 +0100

    Created network manager class

---
+++ MandSTest/Networking/NetworkManager.h
@@ -0,0 +1,13 @@
+//
+//  NetworkManager.h
+//  MandSTest
+//
+//  Created by Trevor Doodes on 13/08/2015.
+//  Copyright (c) 2015 Ironworks Media Limited. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NetworkManager : NSObject
+
+@end
---
+++ MandSTest/Networking/NetworkManager.m
@@ -0,0 +1,13 @@
+//
+//  NetworkManager.m
+//  MandSTest
+//
+//  Created by Trevor Doodes on 13/08/2015.
+//  Copyright (c) 2015 Ironworks Media Limited. All rights reserved.
+//
+
+#import "NetworkManager.h"
+
+@implementation NetworkManager
+
+@end
